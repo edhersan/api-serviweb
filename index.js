2f442557244609dc8d5000a6e23d2d4b9ce0f081
edhersan@gmail.com <edhersangmail.com>
2025-03-28 05:18:57 -0500

    declaro endpoints

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/call', (req, res) => res.send('obtenniendo datos'));
+app.post('/send', (req, res) => res.send('enviando datos'));
+app.put('/update', (req, res) => res.send('actualizando datos'));
+app.delete('/delete', (req, res) => res.send('eliminando datos'));
+
+app.listen(3000, () => console.log('Servidor escuchando en el puerto 3000'));
