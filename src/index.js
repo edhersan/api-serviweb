7a96c7c87a047e0123d42648073c18abae6fd683
edhersan@gmail.com <edhersangmail.com>
2025-03-28 19:31:31 -0500

    un preliminar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
-//import {connection} from './conexion.js';
 import formularioRoutes from './routes/formulario.routes.js';
 
 const app = express();
 
 app.use(formularioRoutes)
-app.listen(3000, () => console.log('Servidor escuchando en el puerto 3000'));
--- src/routes/otra.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-router.get('/ping',async (req, res) => {
-    const RESULTADO = await connection.query('SELECT 1+1 AS RESULTADO')
-    res.json(RESULTADO[0])
- });
-export default router;
